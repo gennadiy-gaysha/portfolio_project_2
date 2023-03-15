942116bd21c57ccd947704ea16882e2e40d81319
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-03-15 10:38:20 +0000

    Add cap to the dice for initial dice state

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,19 @@ const dices = document.getElementsByClassName('dice');
 for (let dice of dices) {
     dice.addEventListener('click', function () {
         if (this.style.backgroundColor === 'yellow') {
-            dice.style.backgroundColor = 'white'
+            this.style.backgroundColor = 'white'
+            this.classList.add('some-class');
         } else {
             this.style.backgroundColor = 'yellow'
+            this.classList.remove('some-class');
         }
     })
 }
 
 const divRoll = document.getElementById('roll--dice')
 divRoll.addEventListener('click', function () {
+    // holdNumber();
+    revealDice();
     const diceImages = document.getElementsByClassName('dice-image')
     for (let diceImage of diceImages) {
         const diceNumber = Math.floor(Math.random() * 6 + 1);
@@ -31,3 +35,24 @@ divRoll.addEventListener('click', function () {
 
     }
 })
+
+function revealDice() {
+    const redDices = document.getElementsByClassName('red');
+    const diceImages = document.getElementsByClassName('dice-image');
+    for (let redDice of redDices) {
+        redDice.classList.add('hidden');
+    }
+    for (let diceImage of diceImages) {
+        diceImage.classList.remove('hidden');
+    }
+}
+
+
+
+function holdNumber() {
+    for (let dice of dices) {
+        if (dice.style.backgroundColor === 'yellow') {
+            console.log(dice.innerHTML);
+        }
+    }
+}
