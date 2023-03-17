5066e570282ca26fb87c1161483cfeecce9a737c
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-03-17 13:53:37 +0000

    Add comments to js script and block change dice color function after the third throw

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.getElementById('start').addEventListener('click', function () {
 
 })
 
-//Hold the button eventListener (number)
+//Hold the dice number on click
 
 const diceImages = document.getElementsByClassName('dice-image');
 for (let diceImage of diceImages) {
@@ -24,51 +24,56 @@ for (let diceImage of diceImages) {
 }
 
 /**
- *  Switching between white and yellow (hold dice) color
- * after clicking "Roll Dice" button by adding eventListener to it
+ *  Switching between white and yellow (hold dice) bg color - become active
+ * after clicking "Roll Dice" button (synchronized with the eventListener above)
  * */
 
 function holdButtonColor() {
     if (this.style.backgroundColor === 'yellow') {
         this.style.backgroundColor = 'white'
-        console.log(this.style.backgroundColor);
     } else {
         this.style.backgroundColor = 'yellow'
     }
 }
 
 
-// Add eventListener to roll--dice button 
-let counter = 3;
+// Add eventListener to "Roll Dice" (#roll--dice) button 
+let counter = 3; //this global variable is used only once when the game starts
 document.getElementById('roll--dice').addEventListener('click', function () {
 
     if (counter > 0) {
+        //Calling this function here allows us to remove the red cap and make the dice numbers visible
         revealDice();
-        const diceImages = document.getElementsByClassName('unhold-dice')
-        for (let diceImage of diceImages) {
 
+        const diceImages = document.getElementsByClassName('unhold-dice');
+        for (let diceImage of diceImages) {
             let diceNumber = Math.floor(Math.random() * 6 + 1);
             const imageURL = `assets/img/${diceNumber}.png`
             diceImage.src = imageURL;
             displayCurrentScore();
         }
-        //This allows us to chage the color (white to yellow and vice versa) of the dice only when 
-        //'Roll dice' button is clicked (but not before!)
+        //Adding this eventListener here allows us to chage the color (from white to yellow and vice versa) 
+        //of the dice only after the 'Roll dice' button is clicked (but not before!)
         const dices = document.getElementsByClassName('dice');
         for (let dice of dices) {
             dice.addEventListener('click', holdButtonColor)
         };
 
+        //Adding this eventListener here allows us to write down (fix) current score only after
+        //the 'Roll dice' button is clicked (but not before!)
         document.getElementById('current-sixes-score').addEventListener('click', addSixes);
     } else {
         alert('Please, choose the line to enter your current score');
-
     }
-
-
     counter -= 1;
-    console.log(counter);
-
+    //Adding this statement here prevents from changing dice color just after the third throw
+    if (counter === 0) {
+        console.log(counter);
+        const dices = document.getElementsByClassName('dice');
+        for (let dice of dices) {
+            dice.removeEventListener('click', holdButtonColor)
+        };
+    }
 })
 
 /** 
@@ -88,6 +93,7 @@ function revealDice() {
 
 /** 
  * Displays current possible score for the line in score table
+ * by retrieving dice number from img src attribute value
  * */
 
 function displayCurrentScore() {
@@ -95,7 +101,6 @@ function displayCurrentScore() {
     for (let diceImage of diceImages) {
         arr.push(diceImage.getAttribute('src'));
     }
-
     let arr2 = arr.map((item) => parseInt(item.match(/\d+/)))
         .filter((item) => item === 6)
         .reduce((acc, val) => acc + val, 0);
@@ -104,21 +109,26 @@ function displayCurrentScore() {
 }
 
 /**
- * Writing down the current score to "Sixes" line after clicking 'Roll Dice' button 
- * and add it to Left and Total lines:
- *  */
-
-let scoreEntered = false;
+ * Assigns value to global variable after the game starts
+ */
 
 function enterScore() {
     counter = 3;
 }
 
-function addSixes() {
-    enterScore();
-    const fixedSixes = document.getElementById('sixes-score');
+/**
+ * Allows to write down the current score to "Sixes" line by clicking on it 
+ * (possible only after clicking 'Roll Dice' button) and add it to Left and Total lines:
+ *  */
 
+function addSixes() {
+    //assigns value 3 to global variable counter
+    enterScore();
+    //After writing down (fixing) the score these elements become visible (display: block)
+    const fixedSixes = document.getElementById('sixes-score');
+    //After writing down (fixing) the score these elements become invisible (display: none)
     const currentSixes = document.getElementById('current-sixes-score');
+
     const sixes = document.getElementById('sixes');
     const left = document.getElementById('left');
     const total = document.getElementById('total');
@@ -126,32 +136,41 @@ function addSixes() {
     fixedSixes.textContent = currentSixes.textContent;
     const fixedSixesNumber = parseInt(fixedSixes.textContent);
 
+    //Adds current score to the "Left" value in the score-container
     let leftNumber = parseInt(left.textContent)
     leftNumber += fixedSixesNumber;
     left.textContent = leftNumber;
 
+    //Adds current score to the "Total" value in the score-container
     let totalNumber = parseInt(total.textContent)
     totalNumber += fixedSixesNumber;
     total.textContent = totalNumber;
 
+    //Changes color of the fixed score line from black to red;
     fixedSixes.style.color = 'red';
     sixes.style.color = 'red';
     fixedSixes.classList.remove('hidden');
     currentSixes.classList.add('hidden');
+    //Calling this function here allows us return dices to the starting point, when they are red-capped,
+    //have white background (.dice) and ready to get random numbers (.dice-image, class="unhold-dice") 
     initialState();
 }
 
+/**
+ * Starting state of the dices at the beginning of each iteration (3 throw in one turn)
+ */
 function initialState() {
+    //white background
     const dices = document.getElementsByClassName('dice');
     for (let dice of dices) {
         dice.style.backgroundColor = 'white';
     }
-
+    //ready to get random numbers
     const diceImages = document.getElementsByClassName('dice-image');
     for (let diceImage of diceImages) {
         diceImage.classList.add('unhold-dice');
     }
-
+    //red capped
     const redDices = document.getElementsByClassName('red');
     for (let redDice of redDices) {
         redDice.classList.remove('hidden');
@@ -159,7 +178,6 @@ function initialState() {
     for (let diceImage of diceImages) {
         diceImage.classList.add('hidden');
     }
-
     // This blocks changing dice color from white to yellow until the "Roll Dice button will be clicked again"
     for (let dice of dices) {
         dice.removeEventListener('click', holdButtonColor)
