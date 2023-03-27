7ea54f73a08c52ccd2790ade37e855922c40546b
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-03-27 15:57:33 +0000

    Add element with id='error-404' from 404.html file to the DOM by creating common.js file

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (error404) {
         error404.addEventListener('click', function () {
-            window.location.href('index.html')
+            window.location.href = 'index.html';
         })
     }
 
